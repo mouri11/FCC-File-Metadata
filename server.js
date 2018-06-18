a5ef68201a707341abec2e69234cb7422fb47ab0
Glitch (fcc-file-upload-mouri11) <none>
2018-06-18 10:07:58 +0000

    server.js: Used multer to upload files and display metadata

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 
 var express = require('express');
 var cors = require('cors');
-var fileUpload = require('express-fileupload');
 
 // require and use "multer"...
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use(fileUpload());
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
@@ -21,14 +21,8 @@ app.get('/hello', function(req, res){
 });
 
 //Handling file uploads
-app.post('/api/fileanalyse', function(req,res) {
-  if (!req.files) return res.status(400).send('No files were uploaded.');
-  
-  let upfile = req.files.upfile;
-  upfile.mv('./', (err) => {
-    if (err) return res.status(500).send(err);
-    res.send('File uploaded!');
-  });
+app.post('/api/fileanalyse', upload.any(), function(req,res,next) {
+  res.json({"name": req.files[0].originalname, "type": req.files[0].mimetype, "size": req.files[0].size});
 });
 
 app.listen(process.env.PORT || 3000, function () {
